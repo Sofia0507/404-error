46e4d53c309b9684a8b7ddaed752ece1728beb54
Sofia0507 <sofiaiglesiasgarcia0@gmail.com>
2025-11-18 23:16:27 +0100

    ufo

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-const numStars = 150;
-
-for (let i = 0; i < numStars; i++) {
-  const star = document.createElement("div");
-  star.classList.add("star");
-  star.style.top = `${Math.random() * 100}vh`;
-  star.style.left = `${Math.random() * 100}vw`;
-  star.style.animationDuration = `${1 + Math.random() * 2}s`;
-  star.style.animationDelay = `${Math.random() * 2}s`;
-  document.body.appendChild(star);
-}
 
 
 const ufo = document.querySelector(".ufo");
 const beam = document.querySelector(".beam");
-document.querySelectorAll(.mode-wrap).forEach(wrap=>){
+document.querySelectorAll(".model-wrap").forEach(wrap => {
+
   wrap.addEventListener("mouseenter", () => {
     const rect = wrap.getBoundingClientRect();
     ufo.style.left = rect.left + rect.width / 2 + "px";
@@ -29,6 +19,28 @@ document.querySelectorAll(.mode-wrap).forEach(wrap=>){
   });
 
 });
+
+const beamText = document.querySelector(".beam-text");
+
+document.querySelectorAll(".model-wrap").forEach(wrap => {
+  wrap.addEventListener("mouseenter", () => {
+    const rect = wrap.getBoundingClientRect();
+    ufo.style.left = rect.left + rect.width / 2 + "px";
+    ufo.style.top = rect.top - 120 + "px";
+
+    beam.classList.add("active");
+    wrap.classList.add("abducting");
+    beamText.classList.add("active"); // activa el texto junto con el haz
+  });
+
+  wrap.addEventListener("mouseleave", () => {
+    beam.classList.remove("active");
+    wrap.classList.remove("abducting");
+    beamText.classList.remove("active"); // lo apaga al salir
+  });
+});
+
+
 
 
 
